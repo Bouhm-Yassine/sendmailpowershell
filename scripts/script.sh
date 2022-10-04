fdeae15af821ac7173e4531730cd5aeb5f9440e7
Bouhm-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-10-04 13:41:18 +0100

    fc

---
+++ scripts/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "paaaaaaaaaaaaaaaaaaaa bbbbbb"
+ls -l /
---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static class ProcessReader implements Callable {
+        private InputStream inputStream;
+
+        public ProcessReader(InputStream in) {
+            this.inputStream = in;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            return new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.toList());
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+
+            boolean isWin = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+        ProcessBuilder builder = new ProcessBuilder();
+        if(isWin) {
+            System.out.println("========= WINDOWS");
+            builder.command("scripts/script.bat");
+        } else {
+            System.out.println("========= UNIX");
+            builder.command("sh", "-c", "scripts/script.sh");
+        }
+
+
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+
+        try {
+            Process process = builder.start();
+            ProcessReader task = new ProcessReader(process.getInputStream());
+            Future<List<String>> future = pool.submit(task);
+
+/*            List<String> results = future.get();
+
+            for(String s: results) {
+                System.out.println(s);
+            }*/
+
+            int exitCode = process.waitFor();
+            System.out.println("======> EXIT CODE: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            pool.shutdown();
+        }
+
+
+        // ======================
+/*            Process process = Runtime.getRuntime().exec("scripts/script.bat");
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if(exitVal == 0) {
+                System.out.println("========> Success");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                System.out.println("========> ERROR");
+            }*/
+    }
+}
